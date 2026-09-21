ef72c5470857761bb90eb7eb4ab35440c624cf68
Rajendra <94388283+rajendrathalluru@users.noreply.github.com>
2026-09-20 21:37:33 -0500

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // Both are empty
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+
+        // One is empty, the other isn't
+        if (p == nullptr || q == nullptr) {
+            return false;
+        }
+
+        // Values are different
+        if (p->val != q->val) {
+            return false;
+        }
+
+        // Compare left and right subtrees
+        return isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
